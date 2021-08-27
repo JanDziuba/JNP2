7bbffb6a71e6d26d90ea79cfb43255b17493a71d
Jan Dziuba <jandziuba53@gmail.com>
2021-08-27 18:38:52 +0200

    fix get request, remove unnecessary corsheaders

--- front/frontapp/views.py
+++ front/frontapp/views.py
@@ -34,7 +34,7 @@ def action_logout(request):
 
 def action_get_offers(request):
     if request.is_ajax and request.method == "GET":
-        response = requests.get('http://host.docker.internal:8002/api/get_offers', data=request.GET)
+        response = requests.get('http://host.docker.internal:8002/api/get_offers', params=request.GET)
         return JsonResponse(response.json(), status=response.status_code)
     return JsonResponse({'error': 'wrong method'}, status=405)
 
@@ -54,14 +54,14 @@ def get_context_from_session(request):
 
 def home_view(request):
     context = get_context_from_session(request)
-    response = requests.get('http://host.docker.internal:8002/api/get_offers', data=request.GET)
+    response = requests.get('http://host.docker.internal:8002/api/get_offers')
     context['offers'] = response.json()['data']
     return render(request, 'frontapp/home.html', context)
 
 
 def offer_detail_view(request, id):
     context = get_context_from_session(request)
-    response = requests.get(f'http://host.docker.internal:8002/api/get_offer/{id}', data=request.GET)
+    response = requests.get(f'http://host.docker.internal:8002/api/get_offer/{id}')
     context['offer'] = response.json()['data']
     return render(request, 'frontapp/offerDetail.html', context)
 
--- offers/offers/settings.py
+++ offers/offers/settings.py
@@ -27,8 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-CORS_ORIGIN_ALLOW_ALL = True
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -39,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'corsheaders',
     'offersapp'
 ]
 
@@ -51,7 +48,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'offers.urls'
